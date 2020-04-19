5f3e1d6ca5a6671d3da618a29a5e7d37f2dd073c
vancouver29 <vancouver2990@gmail.com>
2020-04-19 19:49:01 +0200

    Design UI frontend

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,32 @@
-<?php
+</div><!-- /.blog-main -->
+
+<aside class="col-md-4 blog-sidebar">
+    <div class="p-4 mb-3 bg-light rounded">
+        <h4 class="font-italic">About</h4>
+        <p class="mb-0">Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+    </div>
+
+    <div class="p-4">
+        <h4 class="font-italic">Categories</h4>
+        <ol class="list-unstyled mb-0">
+            <li><a href="#">News</a></li>
+            <li><a href="#">Events</a></li>
+            <li><a href="#">Tutorials</a></li>
+            <li><a href="#">Misc</a></li>
+        </ol>
+    </div>
+
+</aside><!-- /.blog-sidebar -->
+
+</div><!-- /.row -->
+
+</main><!-- /.container -->
+
+<footer class="blog-footer">
+    <p>NewsLoversBlog &copy; 2020</p>
+    <p>
+        <a href="#">Back to top</a>
+    </p>
+</footer>
+</body>
+</html>
--- posts.php
+++ posts.php
@@ -1,1 +1,32 @@
-<?php
+<?php include 'includes/header.php'?>
+
+<div class="blog-post">
+    <h2 class="blog-post-title">International PHP Conference 2020</h2>
+    <p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <a class="readmore" href="post.php?id=1">Readmore</a>
+</div><!-- /.blog-post -->
+
+<div class="blog-post">
+    <h2 class="blog-post-title">10 Tricks for Clean Code</h2>
+    <p class="blog-post-meta">December 23, 2013 by <a href="#">Jacob</a></p>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <a class="readmore" href="post.php?id=2">Readmore</a>
+</div><!-- /.blog-post -->
+
+<div class="blog-post">
+    <h2 class="blog-post-title">New feature</h2>
+    <p class="blog-post-meta">December 14, 2013 by <a href="#">Chris</a></p>
+
+    <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+    <a class="readmore" href="post.php?id=3">Readmore</a>
+</div><!-- /.blog-post -->
+
+<!--            <nav class="blog-pagination">-->
+<!--                <a class="btn btn-outline-primary" href="#">Older</a>-->
+<!--                <a class="btn btn-outline-secondary disabled" href="#" tabindex="-1" aria-disabled="true">Newer</a>-->
+<!--            </nav>-->
+
+<?php include 'includes/footer.php'?>
